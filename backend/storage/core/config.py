c38bd36ff3d706f3057d7a320506b3f219462be5
abhinav-aegis <abhinav@aegis-enterprise.com>
2025-03-31 03:12:34 +0000

    Added storage microservice from template

---
+++ backend/storage/core/config.py
@@ -0,0 +1,12 @@
+from backend.common.core.config import Settings, SettingsConfigDict
+
+class ServiceSettings(Settings):
+    model_config = SettingsConfigDict(
+        case_sensitive=True,
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore"
+    )
+    PROJECT_NAME: str = "Template Microservice"
+
+settings = ServiceSettings()
